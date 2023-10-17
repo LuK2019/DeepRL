2b963132e8ed8019da6200728f37269ce75fe634
lukas.ketzer <lukas.ketzer@ryver.ai>
2023-10-17 08:40:57 -0700

    revert changes to make_config

--- hw3/cs285/scripts/scripting_utils.py
+++ hw3/cs285/scripts/scripting_utils.py
@@ -6,20 +6,13 @@ import cs285.env_configs
 from cs285.infrastructure.logger import Logger
 
 def make_config(config_file: str) -> dict:
-    # Load the given config_file
+    config_kwargs = {}
     with open(config_file, "r") as f:
         config_kwargs = yaml.load(f, Loader=yaml.SafeLoader)
 
     base_config_name = config_kwargs.pop("base_config")
 
-    # Load the base configuration
-    base_config = cs285.env_configs.configs[base_config_name]()
-
-    # Merge the configurations. This ensures that the base configuration
-    # is overwritten by the config_file values.
-    merged_config = {**base_config, **config_kwargs}
-
-    return merged_config
+    return cs285.env_configs.configs[base_config_name](**config_kwargs)
 
 def make_logger(logdir_prefix: str, config: dict) -> Logger:
     data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../data")
